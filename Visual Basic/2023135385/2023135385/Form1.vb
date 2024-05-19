7c66b2dd22d216cb04929d9096fd808fd61db9ac
Adam <51192873+skyscrapercode@users.noreply.github.com>
2024-05-19 13:37:23 +0800

    Add files via upload

---
+++ Visual Basic/2023135385/2023135385/Form1.vb
@@ -0,0 +1,93 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim dblFloorAreaSize, dblFloorTypePrice, dblTotalCost, dblTotalFloorCost, dblTotalCostBeforeLabor As Double
+        Dim intTypeOfFlooring, intBuyerStatus As Integer
+        Dim dblLaborCost As Double = 0
+
+        Double.TryParse(txtFloorSizeInput.Text, dblFloorAreaSize)
+
+        If dblFloorAreaSize <= 0 Then
+            MsgBox("Please Input A Positive Number", 0 Or 16, "Error")
+        Else
+            If radLaminate.Checked = True Then
+                intTypeOfFlooring = 1
+            ElseIf radVinly.Checked = True Then
+                intTypeOfFlooring = 2
+            Else
+                intTypeOfFlooring = 3
+            End If
+
+            dblFloorTypePrice = floorTypePrice(intTypeOfFlooring)
+            dblTotalCostBeforeLabor = calculateFloorCost(dblFloorTypePrice, dblFloorAreaSize, dblLaborCost)
+
+            dblTotalCost = dblTotalCostBeforeLabor + dblLaborCost
+
+            If chkMembership.Checked = True Then
+                intBuyerStatus = 1
+            Else
+                intBuyerStatus = 0
+            End If
+
+            dblTotalFloorCost = totalFloorCost(dblTotalCost, intBuyerStatus)
+
+            displayFloorBill(dblTotalCostBeforeLabor, dblLaborCost, dblTotalFloorCost)
+        End If
+
+
+    End Sub
+
+    Private Function floorTypePrice(ByVal floorType As Integer) As Double
+        Dim flooringPricePerMeter As Double
+
+        If floorType = 1 Then
+            flooringPricePerMeter = 20
+        ElseIf floorType = 2 Then
+            flooringPricePerMeter = 15
+        Else
+            flooringPricePerMeter = 25
+        End If
+
+        Return flooringPricePerMeter
+    End Function
+
+    Private Function calculateFloorCost(ByVal dblFloorPricePerMeter As Double, ByVal dblFloorAreaSize As Double, ByRef dblLaborCost As Double) As Double
+        Dim dblTotalCost As Double
+
+        dblTotalCost = dblFloorAreaSize * dblFloorPricePerMeter
+        dblLaborCost = dblFloorAreaSize * 5
+
+        Return dblTotalCost
+    End Function
+
+    Private Function totalFloorCost(ByVal dblTotalCost As Double, ByVal intBuyerStatus As Integer) As Double
+        Dim dblTotalFloorCost, dblCostAfterDiscount As Double
+
+        If intBuyerStatus = 1 Then
+            dblCostAfterDiscount = dblTotalCost - (dblTotalCost * 0.07)
+            dblTotalFloorCost = dblCostAfterDiscount + (dblCostAfterDiscount * 0.09)
+        Else
+            dblTotalFloorCost = dblTotalCost + (dblTotalCost * 9 / 100)
+        End If
+
+        Return dblTotalFloorCost
+    End Function
+
+    Private Sub displayFloorBill(ByVal dblTotalCost As Double, ByVal dblLaborCost As Double, ByVal dblFinalPrice As Double)
+        lblFlooringCost.Text = dblTotalCost.ToString("C2")
+        lblLaborCost.Text = dblLaborCost.ToString("C2")
+        lblFinalPrice.Text = dblFinalPrice.ToString("C2")
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtFloorSizeInput.Text = String.Empty
+        lblFinalPrice.Text = String.Empty
+        lblFlooringCost.Text = String.Empty
+        lblLaborCost.Text = String.Empty
+        chkMembership.Checked = False
+        txtFloorSizeInput.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
---
+++ Visual Basic/calcSalesPriceRetail/calcSalesPriceRetail/Form1.vb
@@ -0,0 +1,47 @@
+﻿Public Class Form1
+    Dim dblSalePrice, dblRetailPrice As Double
+    Dim decDiscountPercentage As Decimal
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        If ValidateInput() Then
+            dblSalePrice = CalculateSalePrice(dblRetailPrice, decDiscountPercentage)
+            DisplayOutput(dblSalePrice)
+        Else
+            MsgBox("Please Input Correct Values", 0 Or 16, "Error")
+        End If
+
+
+
+    End Sub
+
+    Private Function ValidateInput() As Boolean
+        If Not Double.TryParse(txtRetailPrice.Text, dblRetailPrice) Then ' Another way is you can make TryParse = 0 to check if it's right or wrong
+            lblMessage.Text = "Retail price must be numeric"
+            Return False
+        End If
+
+        If Not Decimal.TryParse(txtDiscountPercent.Text, decDiscountPercentage) Then
+            lblMessage.Text = "Discount Percentage must be numeric"
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    Private Function CalculateSalePrice(ByVal dblRetailPrice As Double, ByVal decDiscountPercentage As Decimal) As Double
+        Dim dblSalePrice As Double
+
+        dblSalePrice = dblRetailPrice - (dblRetailPrice * decDiscountPercentage)
+
+        Return dblSalePrice
+    End Function
+
+    Private Sub DisplayOutput(ByVal dblSalePrice As Double)
+        lblSalePrice.Text = dblSalePrice.ToString("C2")
+    End Sub
+End Class
