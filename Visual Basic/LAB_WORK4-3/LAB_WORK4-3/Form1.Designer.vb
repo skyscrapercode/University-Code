9da2b58813dc7bf1a6a68b994f684b4bd7a1f529
Adam <51192873+skyscrapercode@users.noreply.github.com>
2024-05-19 13:51:47 +0800

    Add files via upload

---
+++ Visual Basic/LAB_WORK4-3/LAB_WORK4-3/Form1.Designer.vb
@@ -0,0 +1,253 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.chkRushDelivery = New System.Windows.Forms.CheckBox()
+        Me.txtSpoolInput = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lblTotalDue = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblShipping = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblBackOrder = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblReadyToShip = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnCalcTotal = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtSpoolInStock = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtSpoolInStock)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.chkRushDelivery)
+        Me.GroupBox1.Controls.Add(Me.txtSpoolInput)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(309, 116)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Order Information"
+        '
+        'chkRushDelivery
+        '
+        Me.chkRushDelivery.AutoSize = True
+        Me.chkRushDelivery.Location = New System.Drawing.Point(9, 84)
+        Me.chkRushDelivery.Name = "chkRushDelivery"
+        Me.chkRushDelivery.Size = New System.Drawing.Size(118, 21)
+        Me.chkRushDelivery.TabIndex = 2
+        Me.chkRushDelivery.Text = "Rush Delivery"
+        Me.chkRushDelivery.UseVisualStyleBackColor = True
+        '
+        'txtSpoolInput
+        '
+        Me.txtSpoolInput.Location = New System.Drawing.Point(186, 33)
+        Me.txtSpoolInput.Name = "txtSpoolInput"
+        Me.txtSpoolInput.Size = New System.Drawing.Size(100, 22)
+        Me.txtSpoolInput.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(186, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Number of Spools Ordered: "
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lblTotalDue)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.lblShipping)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.lblBackOrder)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.lblReadyToShip)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 134)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(309, 202)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Delivery Information"
+        '
+        'lblTotalDue
+        '
+        Me.lblTotalDue.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotalDue.Location = New System.Drawing.Point(160, 124)
+        Me.lblTotalDue.Name = "lblTotalDue"
+        Me.lblTotalDue.Size = New System.Drawing.Size(100, 23)
+        Me.lblTotalDue.TabIndex = 6
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(80, 125)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(74, 17)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Total Due:"
+        '
+        'lblShipping
+        '
+        Me.lblShipping.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblShipping.Location = New System.Drawing.Point(160, 94)
+        Me.lblShipping.Name = "lblShipping"
+        Me.lblShipping.Size = New System.Drawing.Size(100, 23)
+        Me.lblShipping.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 94)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(155, 17)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Shipping and Handling:"
+        '
+        'lblBackOrder
+        '
+        Me.lblBackOrder.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblBackOrder.Location = New System.Drawing.Point(160, 67)
+        Me.lblBackOrder.Name = "lblBackOrder"
+        Me.lblBackOrder.Size = New System.Drawing.Size(100, 23)
+        Me.lblBackOrder.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 68)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(151, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Spools on Back Order:"
+        '
+        'lblReadyToShip
+        '
+        Me.lblReadyToShip.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblReadyToShip.Location = New System.Drawing.Point(160, 40)
+        Me.lblReadyToShip.Name = "lblReadyToShip"
+        Me.lblReadyToShip.Size = New System.Drawing.Size(100, 23)
+        Me.lblReadyToShip.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(148, 17)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Spools Ready to Ship:"
+        '
+        'btnCalcTotal
+        '
+        Me.btnCalcTotal.Location = New System.Drawing.Point(18, 356)
+        Me.btnCalcTotal.Name = "btnCalcTotal"
+        Me.btnCalcTotal.Size = New System.Drawing.Size(124, 43)
+        Me.btnCalcTotal.TabIndex = 7
+        Me.btnCalcTotal.Text = "Calculate Total"
+        Me.btnCalcTotal.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(148, 356)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(124, 43)
+        Me.btnClear.TabIndex = 8
+        Me.btnClear.Text = "Clear Form"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(278, 356)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(124, 43)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(6, 64)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(183, 17)
+        Me.Label6.TabIndex = 3
+        Me.Label6.Text = "Number of Spools In Stock: "
+        '
+        'txtSpoolInStock
+        '
+        Me.txtSpoolInStock.Location = New System.Drawing.Point(186, 61)
+        Me.txtSpoolInStock.Name = "txtSpoolInStock"
+        Me.txtSpoolInStock.Size = New System.Drawing.Size(100, 22)
+        Me.txtSpoolInStock.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(412, 429)
+        Me.Controls.Add(Me.btnCalcTotal)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Order Status"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents chkRushDelivery As CheckBox
+    Friend WithEvents txtSpoolInput As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents lblTotalDue As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblShipping As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblBackOrder As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblReadyToShip As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnCalcTotal As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtSpoolInStock As TextBox
+    Friend WithEvents Label6 As Label
+End Class
