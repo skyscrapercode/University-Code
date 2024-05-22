4e3496ad4f15e3e1249ee2f964c822b7b34e95e0
Adam <51192873+skyscrapercode@users.noreply.github.com>
2024-05-22 14:04:59 +0800

    Add more projects

---
+++ Visual Basic/learningListBox/learningListBox/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.lstOutput2 = New System.Windows.Forms.ListBox()
+        Me.btnOk = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(544, 114)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnter.TabIndex = 0
+        Me.btnEnter.Text = "Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(544, 156)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.ItemHeight = 16
+        Me.lstOutput.Items.AddRange(New Object() {"January", "Febuary", "March", "April"})
+        Me.lstOutput.Location = New System.Drawing.Point(12, 12)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(109, 116)
+        Me.lstOutput.TabIndex = 2
+        '
+        'lstOutput2
+        '
+        Me.lstOutput2.FormattingEnabled = True
+        Me.lstOutput2.ItemHeight = 16
+        Me.lstOutput2.Items.AddRange(New Object() {"2020", "2021", "2022", "2023", "2024"})
+        Me.lstOutput2.Location = New System.Drawing.Point(127, 12)
+        Me.lstOutput2.Name = "lstOutput2"
+        Me.lstOutput2.Size = New System.Drawing.Size(128, 116)
+        Me.lstOutput2.TabIndex = 3
+        '
+        'btnOk
+        '
+        Me.btnOk.Location = New System.Drawing.Point(544, 68)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(75, 23)
+        Me.btnOk.TabIndex = 4
+        Me.btnOk.Text = "OK"
+        Me.btnOk.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(463, 156)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 5
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(649, 324)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnOk)
+        Me.Controls.Add(Me.lstOutput2)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lstOutput As ListBox
+    Friend WithEvents lstOutput2 As ListBox
+    Friend WithEvents btnOk As Button
+    Friend WithEvents btnReset As Button
+End Class
---
+++ Visual Basic/projectM/projectM/BirthdaySong.vb
@@ -0,0 +1,19 @@
+﻿Public Class BirthdaySong
+    Private Sub AxWindowsMediaPlayer1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim videoFolderPath As String = IO.Path.Combine(Application.StartupPath, "Resources")
+        Dim videoFilePath As String = IO.Path.Combine(videoFolderPath, "happy_birthday.mp4")
+        AxWindowsMediaPlayer1.URL = videoFilePath
+
+        AxWindowsMediaPlayer1.Ctlcontrols.stop()
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.stop()
+        Me.Close()
+    End Sub
+
+    Private Sub BirthdaySong_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        AxWindowsMediaPlayer1.Ctlcontrols.stop()
+    End Sub
+
+End Class
